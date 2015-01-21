6358038f77b2064bdfc1a7c6d342973d7b96ad29
Kevin Martensson <kevinmartensson@gmail.com>
2015-01-22 00:35:05 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var getProxy = require('./');
+var test = require('ava');
+
+test('get configured proxy', function (t) {
+	var proxy = process.env.HTTP_PROXY;
+	process.env.HTTP_PROXY = 'http://192.168.0.1:8080';
+	t.assert(getProxy() === 'http://192.168.0.1:8080');
+	process.env.HTTP_PROXY = proxy;
+	t.end();
+});
