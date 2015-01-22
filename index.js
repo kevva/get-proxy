0448cb6ab77c2b0dffaa3bda1e59d83aec5f750e
Kevin Martensson <kevinmartensson@gmail.com>
2015-01-22 18:00:37 +0100

    Check ENV variables before `.npmrc`

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ var conf = require('rc')('npm');
  */
 
 module.exports = function () {
-	return conf['https-proxy'] ||
-		conf['http-proxy'] ||
-		conf.proxy ||
-		process.env.HTTPS_PROXY ||
+	return process.env.HTTPS_PROXY ||
 		process.env.https_proxy ||
 		process.env.HTTP_PROXY ||
 		process.env.http_proxy ||
+		conf['https-proxy'] ||
+		conf['http-proxy'] ||
+		conf.proxy ||
 		null;
 };
