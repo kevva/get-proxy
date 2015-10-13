6e2374fd47b1b224b1c110e9cb4e9382aabbc924
Kevin Martensson <kevinmartensson@gmail.com>
2015-10-13 17:01:01 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,12 +1,10 @@
-'use strict';
+import test from 'ava';
+import fn from './';
 
-var test = require('ava');
-var getProxy = require('./');
-
-test('get configured proxy', function (t) {
-	var proxy = process.env.HTTP_PROXY;
+test('get configured proxy', t => {
+	const proxy = process.env.HTTP_PROXY;
 	process.env.HTTP_PROXY = 'http://192.168.0.1:8080';
-	t.assert(getProxy() === 'http://192.168.0.1:8080');
+	t.assert(fn() === 'http://192.168.0.1:8080');
 	process.env.HTTP_PROXY = proxy;
 	t.end();
 });
